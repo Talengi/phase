d9d2e18b8b6ad10ebc8969a0b32a49432e4cf294
David Larlet <larlet@gmail.com>
2013-06-05 12:32:06 +0200

    More elegant jquery, thanks @vinyll!

--- EDMS/static/js/project.js
+++ EDMS/static/js/project.js
@@ -52,22 +52,10 @@ jQuery(function($) {
         var direction = (sortBy == $sortBy.val()) ? '-' : '';
         $('#id_sort_by').val(direction + sortBy);
         $i = $this.children();
-        if ($i.hasClass("icon-chevron-up") ||
-            $i.hasClass("icon-chevron-down")) {
-            if ($i.hasClass("icon-chevron-up")) {
-                $i.removeClass("icon-chevron-up");
-                $i.addClass("icon-chevron-down");
-            } else {
-                $i.removeClass("icon-chevron-down");
-                $i.addClass("icon-chevron-up");
-            }
-        } else {
-            $("#documents th i").each(function() {
-                $(this).removeClass("icon-chevron-down")
-                       .removeClass("icon-chevron-up");
-            });
-            $i.addClass("icon-chevron-down");
-        }
+        $i.is('[class*=icon-chevron-]')
+            ? $i.toggleClass("icon-chevron-up").toggleClass("icon-chevron-down")
+            : $i.addClass('icon-chevron-down');
+        $("#documents th i").not($i).removeClass("icon-chevron-down icon-chevron-up");
         serializeTable(evt);
     });
 
