42b57f2864617a3594309a566cd0da31c9dd9fb1
Thibault Jouannic <thibault@miximum.fr>
2013-09-12 10:48:22 +0200

    Unregister useless default admin module

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
 from models import Document
 
 admin.site.register(Document)
+
+# Unregister useless default admin modules
+admin.site.unregister(Site)
