b3e479437f1224f813dad610c4963aaf4be20303
Thibault Jouannic <thibault@miximum.fr>
2013-09-13 12:25:38 +0200

    Disable useless admin modules

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -3,10 +3,9 @@
 
 from django.contrib import admin
 from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group
 
-from models import Document
-
-admin.site.register(Document)
 
 # Unregister useless default admin modules
 admin.site.unregister(Site)
+admin.site.unregister(Group)
