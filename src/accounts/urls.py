63b0cbcbe36968e434ea2204a348351c782b4d6a
Thibault Jouannic <thibault@miximum.fr>
2013-09-23 10:30:18 +0200

    Create urls for password reset actions

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -11,4 +11,10 @@ urlpatterns = patterns(
     url('^logout/$',
         'django.contrib.auth.views.logout_then_login',
         name='logout'),
+    url('^password-reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>[\d\w-]+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url('^password-reset-complete/$',
+        'django.contrib.auth.views.password_reset_complete',
+        name='password_reset_complete'),
 )
