28f380ba04dd5cd9f5946fb9bb92b50338c45528
Thibault Jouannic <thibault@miximum.fr>
2013-09-24 10:20:20 +0200

    Activating admin module for groups

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -3,9 +3,7 @@
 
 from django.contrib import admin
 from django.contrib.sites.models import Site
-from django.contrib.auth.models import Group
 
 
 # Unregister useless default admin modules
 admin.site.unregister(Site)
-admin.site.unregister(Group)
