54d8290b73927985c2df885da168b56c92962630
Thibault Jouannic <thibault@miximum.fr>
2013-09-24 10:46:11 +0200

    Testing document controller navbar

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission
 
 from accounts.factories import UserFactory
 
@@ -9,6 +10,7 @@ class NavbarTests(TestCase):
     def setUp(self):
         self.user = UserFactory(name='User', password='pass')
         self.url = '/'
+        self.dc_perms = Permission.objects.filter(codename__endswith='_document')
 
     def test_anonymous_navbar(self):
         res = self.client.get(self.url, follow=True)
@@ -25,6 +27,20 @@ class NavbarTests(TestCase):
         self.assertContains(res, 'href="/favorites/"')
         self.assertContains(res, 'href="/">Documents')
         self.assertNotContains(res, 'href="/create/"')
+        self.assertNotContains(res, 'href="/admin/"')
+
+    def test_document_controller_navbar(self):
+        self.user.user_permissions = self.dc_perms
+        self.user.save()
+        self.client.login(username=self.user.email, password='pass')
+
+        self.assertTrue(self.user.is_authenticated())
+        self.assertTrue(self.user.has_perm('documents.add_document'))
+
+        res = self.client.get(self.url, follow=True)
+        self.assertContains(res, 'href="/favorites/"')
+        self.assertContains(res, 'href="/">Documents')
+        self.assertContains(res, 'href="/create/"')
         self.assertNotContains(res, 'href="/admin/"')
 
     def test_admin_navbar(self):
