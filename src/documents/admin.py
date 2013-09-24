b3b96d3dea08333420c04e3d29daec3b574cee35
Thibault Jouannic <thibault@miximum.fr>
2013-09-24 14:26:12 +0200

    Nothing is unregistered anymore (for now)

--- src/documents/admin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from django.contrib.sites.models import Site
-
-
-# Unregister useless default admin modules
-admin.site.unregister(Site)
