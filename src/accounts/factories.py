9ed0bd383cd34c0f28a4eaee8bd870a3f1b56c1c
Thibault Jouannic <thibault@miximum.fr>
2013-11-04 09:49:21 +0100

    Add required default category in user factory

--- src/accounts/factories.py
+++ src/accounts/factories.py
@@ -14,11 +14,18 @@ class UserFactory(factory.DjangoModelFactory):
     @classmethod
     def _prepare(cls, create, **kwargs):
         password = kwargs.pop('password', None)
+        category = kwargs.pop('category', None)
         user = super(UserFactory, cls)._prepare(create, **kwargs)
         if password:
             user.set_password(password)
             if create:
                 user.save()
+
+        # Users MUST belong to at least one category
+        if not category:
+            category = CategoryFactory()
+        category.users.add(user)
+
         return user
 
 
