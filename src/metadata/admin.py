646a82e2627f3778d04727d5dbe3859e7257b980
Thibault Jouannic <thibault@miximum.fr>
2013-11-05 16:35:47 +0100

    Dummy metadata model to test configurable choice fields

--- src/metadata/admin.py
+++ src/metadata/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ValuesList, ListEntry
+from .models import ValuesList, ListEntry, ContractorDeliverable
 
 
 class EntryInline(admin.TabularInline):
@@ -14,3 +14,4 @@ class ValuesListAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ValuesList, ValuesListAdmin)
+admin.site.register(ContractorDeliverable)
