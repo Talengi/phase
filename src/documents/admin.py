8549643d4a7803eee49a1af59e75dbc33677d9ec
Thibault Jouannic <thibault@miximum.fr>
2013-11-07 10:59:11 +0100

    Key to contenttype in category template

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from documents.models import CategoryTemplate
+from .models import CategoryTemplate
+from .admin_forms import CategoryTemplateAdminForm
 
 
 class CategoryTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
+    form = CategoryTemplateAdminForm
 
 
 admin.site.register(CategoryTemplate, CategoryTemplateAdmin)
---
+++ src/documents/admin_forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.contenttypes.models import ContentType
+
+from .models import CategoryTemplate
+
+
+class CategoryTemplateAdminForm(forms.ModelForm):
+    """Custom form for category template. For admin use only."""
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryTemplateAdminForm, self).__init__(*args, **kwargs)
+
+        # Define a custom queryset to limit the available
+        # content types to actual Metadata models
+        qs = ContentType.objects \
+            .filter(app_label='metadata')
+        self.fields['metadata_model'].queryset = qs
+
+    class Meta:
+        model = CategoryTemplate
