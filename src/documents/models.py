b3dc152e5a4c88556837c8032b49d73017bd123c
Thibault Jouannic <thibault@miximum.fr>
2013-11-08 15:15:11 +0100

    Error in import

--- src/documents/models.py
+++ src/documents/models.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.conf import settings
-from django.core.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
