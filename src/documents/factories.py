53d236af3558bf75072fe185aada4a14b6e15c7f
Thibault Jouannic <thibault@miximum.fr>
2013-12-20 16:18:48 +0100

    Fix basic document factory

--- src/documents/factories.py
+++ src/documents/factories.py
@@ -1,8 +1,11 @@
 import datetime
+
+from django.contrib.contenttypes.models import ContentType
 import factory
 from factory.fuzzy import FuzzyDate
 
-from .models import Document, DocumentRevision
+from .models import Document
+from document_types.models import ContractorDeliverable
 
 
 fuzzy_date = FuzzyDate(datetime.date(2012, 1, 1))
@@ -11,15 +14,20 @@ fuzzy_date = FuzzyDate(datetime.date(2012, 1, 1))
 class DocumentFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Document
 
-    title = factory.Sequence(lambda n: 'Document {0}'.format(n))
-    sequencial_number = factory.Sequence(lambda n: '{0}'.format(n))
+    document_key = factory.Sequence(lambda n: 'document-{0}'.format(n))
     current_revision_date = fuzzy_date.fuzz()
-    current_revision = '00'
+    current_revision = 1
 
-
-class RevisionFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = DocumentRevision
-
-    revision = factory.Sequence(lambda n: '{0}'.format(n))
-    revision_date = fuzzy_date.fuzz()
-    document = factory.SubFactory(DocumentFactory)
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        if not 'metadata' in kwargs:
+            kwargs.update({
+                'metadata': ContentType.objects.get_for_model(ContractorDeliverable)
+            })
+        return super(DocumentFactory, cls)._prepare(create, **kwargs)
+#class RevisionFactory(factory.DjangoModelFactory):
+#    FACTORY_FOR = DocumentRevision
+#
+#    revision = factory.Sequence(lambda n: '{0}'.format(n))
+#    revision_date = fuzzy_date.fuzz()
+#    document = factory.SubFactory(DocumentFactory)
