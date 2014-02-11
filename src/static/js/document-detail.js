6546cbe04dc6e69b73db9af6783b6f8149ed1ccc
Thibault Jouannic <thibault@miximum.fr>
2014-02-11 10:41:44 +0100

    BackToTop link: only on document detail

--- src/static/js/document-detail.js
+++ src/static/js/document-detail.js
@@ -30,5 +30,33 @@ jQuery(function($) {
         $(this).closest('div').find('.fileupload-preview').html('');
     });
 
+    // Hide and show "back to top" link on scroll events
+    var configureBackToTopLink = function() {
+        var $window = $(window);
+        var topLink = $('#back-to-top');
+        var scrollTimeout;
+
+        // Actual scroll event handler
+        var scrollHandler = function() {
+            if($window.scrollTop() === 0) {
+                topLink.fadeOut();
+            } else {
+                topLink.fadeIn();
+            }
+        };
+        scrollTimeout = setTimeout(scrollHandler, 250);
+
+        // Debounce the scroll handler using timeouts
+        // See https://github.com/shichuan/javascript-patterns/blob/master/jquery-patterns/window-scroll-event.html
+        $window.scroll(function() {
+            if (scrollTimeout) {
+                clearTimeout(scrollTimeout);
+                scrollTimeout = null;
+            }
+            scrollTimeout = setTimeout(scrollHandler, 250);
+        });
+    };
+    configureBackToTopLink();
+
     $('body').scrollspy({ target: '#document-sidebar' });
 });
