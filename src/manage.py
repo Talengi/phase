ce00c02b91f2902f997229f930a1987e9f4477f7
Thibault Jouannic <thibault@miximum.fr>
2014-02-13 12:04:00 +0100

    Checking that test module is enabled in manage.py

--- src/manage.py
+++ src/manage.py
@@ -5,6 +5,19 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
+    # Let's check that the correct settings module is used
+    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE')
+    if 'test' in sys.argv and not settings_module.endswith('test'):
+        confirm = raw_input("""
+WOW!
+
+It looks like you are running tests, but you did'nt set the
+testing settings module.
+
+Are you sure you want to continue? [y/N] """)
+        if confirm != 'y':
+            sys.exit()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
