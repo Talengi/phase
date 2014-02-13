0da52461fac297a14a79226be9cb6f7ebbae01e3
Thibault Jouannic <thibault@miximum.fr>
2014-02-13 17:01:38 +0100

    Use strict in document detail js

--- src/static/js/document-detail.js
+++ src/static/js/document-detail.js
@@ -1,5 +1,7 @@
 jQuery(function($) {
 
+    "use strict";
+
     /* Disable the whole form */
     $('#document-detail input, #document-detail textarea, #document-detail select')
         .each(function(ev) {
@@ -34,7 +36,6 @@ jQuery(function($) {
     var configureBackToTopLink = function() {
         var $window = $(window);
         var topLink = $('#back-to-top');
-        var scrollTimeout;
 
         // Actual scroll event handler
         var scrollHandler = function() {
@@ -46,7 +47,7 @@ jQuery(function($) {
         };
 
         // Binding to the scroll event is awful for performances
-        scrollInterval = setInterval(scrollHandler, 3000);
+        var scrollInterval = setInterval(scrollHandler, 3000);
     };
     configureBackToTopLink();
 
