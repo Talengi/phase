66aa1deaecbf97efc0bb72e5db836ae0e1e488c4
Thibault Jouannic <thibault@miximum.fr>
2014-02-14 14:07:27 +0100

    Create correspondence model

--- src/default_documents/models.py
+++ src/default_documents/models.py
@@ -7,7 +7,7 @@ from accounts.models import User
 from documents.models import Metadata, MetadataRevision
 from documents.fields import RevisionFileField
 from documents.constants import (
-    BOOLEANS, CLASSES
+    BOOLEANS, CLASSES, CORRESPONDENCE_STATUSES
 )
 from .validators import StringNumberValidator
 
@@ -284,101 +284,148 @@ class ContractorDeliverableRevision(MetadataRevision):
         null=True, blank=True)
 
 
-#class Correspondence(Metadata):
-#    latest_revision = models.ForeignKey(
-#        'CorrespondenceRevision',
-#        verbose_name=_('Latest revision'))
-#
-#    reference = models.CharField(
-#        _('Reference'),
-#        max_length=30)
-#
-#    # General information
-#    subject = models.TextField(_('Subject'))
-#    correspondence_date = models.DateField(_('Correspondence date'))
-#    received_sent_date = models.DateField(_('Received / sent date'))
-#    contract_number = ConfigurableChoiceField(
-#        _('Contract Number'),
-#        max_length=8,
-#        list_index='CONTRACT_NBS')
-#    originator = ConfigurableChoiceField(
-#        _('Originator'),
-#        default='FWF',
-#        max_length=3,
-#        list_index='ORIGINATORS')
-#    recipient = ConfigurableChoiceField(
-#        _('Recipient'),
-#        default='FWF',
-#        max_length=3,
-#        list_index='RECIPIENTS')
-#    document_type = ConfigurableChoiceField(
-#        _('Document Type'),
-#        default="PID",
-#        max_length=3,
-#        list_index='DOCUMENT_TYPES')
-#    sequential_number = models.CharField(
-#        _('Sequential Number'),
-#        default=u"0001",
-#        max_length=4,
-#        choices=SEQUENTIAL_NUMBERS)
-#    author = ConfigurableChoiceField(
-#        _('Author'),
-#        null=True,
-#        blank=True,
-#        max_length=250,
-#        list_index='AUTHORS')
-#    addresses = ConfigurableChoiceField(
-#        _('Addresses'),
-#        null=True,
-#        blank=True,
-#        list_index='ADDRESSES')
-#    response_required = models.NullBooleanField(
-#        _('Response required'),
-#        null=True,
-#        blank=True)
-#    due_date = models.DateField(
-#        _('Due date'),
-#        null=True,
-#        blank=True)
-#
-#    # Revision
-#    current_revision = ConfigurableChoiceField(
-#        _('Revision'),
-#        default='00',
-#        max_length=2,
-#        list_index='REVISIONS')
-#    current_revision_date = models.DateField(_('Revision Date'))
-#
-#    # Related documents
-#    related_documents = models.ManyToManyField(
-#        'documents.Document',
-#        related_name='correspondence_related_set',
-#        null=True, blank=True)
-#
-#    class Meta:
-#        ordering = ('id',)
-#
-#    class PhaseConfig:
-#        pass
-#
-#    def generate_document_key(self):
-#        return slugify(
-#            u"{contract_number}-{originator}-{recipient}"
-#            u"{document_type}-{sequential_number}"
-#            .format(
-#                contract_number=self.contract_number,
-#                originator=self.originator,
-#                recipient=self.recipient,
-#                document_type=self.document_type,
-#                sequential_number=self.sequential_number
-#            )).upper()
-#
-#    def natural_key(self):
-#        return self.reference
-#
-#
-#class CorrespondenceRevision(MetadataRevision):
-#    pass
+class Correspondence(Metadata):
+    latest_revision = models.ForeignKey(
+        'CorrespondenceRevision',
+        verbose_name=_('Latest revision'))
+
+    reference = models.CharField(
+        _('Reference'),
+        max_length=30)
+
+    # General information
+    subject = models.TextField(_('Subject'))
+    correspondence_date = models.DateField(_('Correspondence date'))
+    received_sent_date = models.DateField(_('Received / sent date'))
+    contract_number = ConfigurableChoiceField(
+        _('Contract Number'),
+        max_length=8,
+        list_index='CONTRACT_NBS')
+    originator = ConfigurableChoiceField(
+        _('Originator'),
+        default='FWF',
+        max_length=3,
+        list_index='ORIGINATORS')
+    recipient = ConfigurableChoiceField(
+        _('Recipient'),
+        max_length=3,
+        list_index='RECIPIENTS')
+    document_type = ConfigurableChoiceField(
+        _('Document Type'),
+        default="PID",
+        max_length=3,
+        list_index='DOCUMENT_TYPES')
+    sequential_number = models.CharField(
+        verbose_name=u"sequential Number",
+        help_text=_('Type in a four digit number'),
+        default=u"0001",
+        max_length=4,
+        validators=[StringNumberValidator(4)])
+    author = ConfigurableChoiceField(
+        _('Author'),
+        null=True,
+        blank=True,
+        max_length=250,
+        list_index='AUTHORS')
+    addresses = ConfigurableChoiceField(
+        _('Addresses'),
+        null=True,
+        blank=True,
+        list_index='ADDRESSES')
+    response_required = models.NullBooleanField(
+        _('Response required'),
+        null=True,
+        blank=True)
+    due_date = models.DateField(
+        _('Due date'),
+        null=True,
+        blank=True)
+
+    # Revision
+    current_revision = ConfigurableChoiceField(
+        _('Revision'),
+        default='00',
+        max_length=2,
+        list_index='REVISIONS')
+    current_revision_date = models.DateField(_('Revision Date'))
+
+    # Related documents
+    related_documents = models.ManyToManyField(
+        'documents.Document',
+        related_name='correspondence_related_set',
+        null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = (
+            (
+                "contract_number", "originator", "recipient",
+                "document_type", "sequential_number",
+            ),
+        )
+
+    class PhaseConfig:
+        filter_fields = (
+            'originator', 'recipient', 'status', 'under_review',
+            'overdue', 'leader'
+        )
+        searchable_fields = ('reference', 'subject')
+        column_fields = (
+            ('Reference', 'document_key', 'document_key'),
+            ('Subject', 'subject', 'subject'),
+            ('Rec./Sent date', 'received_sent_date', 'received_sent_date'),
+            ('Resp. required', 'response_required', 'response_required'),
+            ('Due date', 'due_date', 'due_date'),
+            ('Status', 'status', 'latest_revision.status'),
+            ('Under review', 'status', 'latest_revision.status'),
+            ('Overdue', 'overdue', 'latest_revision.overdue'),
+            ('Leader', 'leader', 'latest_revision.leader'),
+            ('Originator', 'originator', 'originator'),
+            ('Recipient', 'recipient', 'recipient'),
+            ('Document type', 'document_type', 'document_type'),
+        )
+
+    def generate_document_key(self):
+        return slugify(
+            u"{contract_number}-{originator}-{recipient}"
+            u"{document_type}-{sequential_number}"
+            .format(
+                contract_number=self.contract_number,
+                originator=self.originator,
+                recipient=self.recipient,
+                document_type=self.document_type,
+                sequential_number=self.sequential_number
+            )).upper()
+
+    def natural_key(self):
+        return (self.reference,)
+
+
+class CorrespondenceRevision(MetadataRevision):
+    status = models.CharField(
+        verbose_name=_('Status'),
+        choices=CORRESPONDENCE_STATUSES,
+        max_length=20,
+        null=True, blank=True)
+    native_file = RevisionFileField(
+        _('Native File'),
+        null=True, blank=True)
+    pdf_file = RevisionFileField(
+        _('PDF File'),
+        null=True, blank=True)
+    under_review = models.NullBooleanField(
+        _('Under Review'),
+        choices=BOOLEANS,
+        null=True, blank=True)
+    overdue = models.NullBooleanField(
+        _('Overdue'),
+        choices=BOOLEANS,
+        null=True, blank=True)
+    leader = models.ForeignKey(
+        User,
+        verbose_name=_('Leader'),
+        related_name='leading_correspondance',
+        null=True, blank=True)
 
 
 class Transmittals(Metadata):
