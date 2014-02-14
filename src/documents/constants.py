cec876634f33b5492926f3d85ba0d26ef350cde8
Thibault Jouannic <thibault@miximum.fr>
2014-02-14 14:06:50 +0100

    Correspondence status constant

--- src/documents/constants.py
+++ src/documents/constants.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
+CORRESPONDENCE_STATUSES = [
+    ('opened', 'Opened'),
+    ('closed', 'Closed'),
+    ('hold', 'Hold'),
+]
+
 CLASSES = [
     (classe, str(classe)) for classe in range(1, 5)
 ]
