57aa76617ee80acd1f132c1db707609fb9ebf817
Thibault Jouannic <thibault@miximum.fr>
2014-02-26 15:22:11 +0100

    Favorite table scroll.
    Make it similar to document list scroll.

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -51,6 +51,7 @@ CACHES = {
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
     'debug_toolbar',
+    'epc1_documents',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
