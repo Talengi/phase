6fc2f9fd71c213a5d202570af4212843a3acd1cd
Thibault Jouannic <thibault@miximum.fr>
2014-04-07 11:04:21 +0200

    Fetch all the reviewables document types

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -2,26 +2,42 @@ from django.views.generic import ListView
 from django.db.models import Q
 
 from accounts.views import LoginRequiredMixin
-from documents.utils import get_all_document_classes
-from default_documents.models import ContractorDeliverable
-#from reviews.models import ReviewMixin
+from documents.utils import get_all_revision_classes
+from reviews.models import ReviewMixin
 
 
 class DocumentList(LoginRequiredMixin, ListView):
     template_name = 'reviews/document_list.html'
-    context_object_name = 'documents'
+    context_object_name = 'revisions'
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentList, self).get_context_data(**kwargs)
+        context.update({
+            'reviews_active': True,
+        })
+        return context
 
     def get_queryset(self):
+        """Get all documents under review.
+
+        Since documents can be of differente types, we need to launch a query
+        for every document type that is reviewable.  We assume that the number
+        of reviewable document classes will never be too high, so we don't do
+        anything to optimize performances for now.
+
+        """
         user = self.request.user
-        q_reviewers = Q(latest_revision__reviewers=user)
-        q_approver = Q(latest_revision__approver=user)
-        q_leader = Q(latest_revision__leader=user)
+        q_reviewers = Q(reviewers=user)
+        q_approver = Q(approver=user)
+        q_leader = Q(leader=user)
 
-        #klasses = get_all_document_classes()
-        #documents = []
-        #for klass in klasses:
-        #    import pdb; pdb.set_trace()
-        #    pass
+        revisions = []
+        klasses = [klass for klass in get_all_revision_classes() if issubclass(klass, ReviewMixin)]
 
-        documents = ContractorDeliverable.objects.filter(q_reviewers|q_approver|q_leader)
-        return documents
+        for klass in klasses:
+            qs = klass.objects \
+                .filter(q_reviewers | q_approver | q_leader) \
+                .select_related('document')
+            revisions += list(qs)
+
+        return revisions
