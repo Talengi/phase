e5852fb481780351f365a784ebd240246ca84719
Thibault Jouannic <thibault@miximum.fr>
2014-04-07 16:09:39 +0200

    Create queries for three review steps

--- src/reviews/tests/test_views.py
+++ src/reviews/tests/test_views.py
@@ -6,7 +6,7 @@ from documents.factories import DocumentFactory
 from accounts.factories import UserFactory
 
 
-class ReviewListTests(TestCase):
+class ReviewersDocumentListTests(TestCase):
 
     def setUp(self):
         self.category = CategoryFactory()
@@ -16,8 +16,12 @@ class ReviewListTests(TestCase):
             is_superuser=True,
             category=self.category
         )
+        self.other_user = UserFactory(
+            email='test@phase.fr',
+            category=self.category
+        )
         self.client.login(email=self.user.email, password='pass')
-        self.url = reverse('review_document_list')
+        self.url = reverse('reviewers_review_document_list')
 
     def test_empty_review_list(self):
         res = self.client.get(self.url)
@@ -27,9 +31,9 @@ class ReviewListTests(TestCase):
         """If the review is not started yet, the doc does not appear in list."""
         doc = DocumentFactory(
             revision={
-                'leader': self.user,
-                'approver': self.user,
                 'reviewers': [self.user],
+                'leader': self.other_user,
+                'approver': self.other_user,
             }
         )
         self.assertFalse(doc.latest_revision.is_under_review())
@@ -39,9 +43,9 @@ class ReviewListTests(TestCase):
     def test_review_started(self):
         doc = DocumentFactory(
             revision={
+                'reviewers': [self.user],
                 'leader': self.user,
                 'approver': self.user,
-                'reviewers': [self.user],
             }
         )
         doc.latest_revision.start_review()
@@ -49,16 +53,163 @@ class ReviewListTests(TestCase):
         res = self.client.get(self.url)
         self.assertContains(res, '<td class="columndocument_key"')
 
+    def test_step_finished(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.user],
+                'leader': self.user,
+                'approver': self.user,
+            }
+        )
+        doc.latest_revision.start_review()
+        doc.latest_revision.end_reviewers_step()
+        self.assertTrue(doc.latest_revision.is_under_review())
+        res = self.client.get(self.url)
+        self.assertNotContains(res, '<td class="columndocument_key"')
+
     def test_review_finished(self):
         doc = DocumentFactory(
             revision={
+                'reviewers': [self.user],
                 'leader': self.user,
                 'approver': self.user,
-                'reviewers': [self.user],
             }
         )
         doc.latest_revision.start_review()
         doc.latest_revision.end_review()
         self.assertFalse(doc.latest_revision.is_under_review())
+        res = self.client.get(self.url)
+        self.assertNotContains(res, '<td class="columndocument_key"')
+
+
+class LeaderDocumentListTests(TestCase):
+
+    def setUp(self):
+        self.category = CategoryFactory()
+        self.user = UserFactory(
+            email='testadmin@phase.fr',
+            password='pass',
+            is_superuser=True,
+            category=self.category
+        )
+        self.other_user = UserFactory(
+            email='test@phase.fr',
+            category=self.category
+        )
+        self.client.login(email=self.user.email, password='pass')
+        self.url = reverse('leader_review_document_list')
+
+    def test_empty_review_list(self):
+        res = self.client.get(self.url)
+        self.assertNotContains(res, '<td class="columndocument_key"')
+
+    def test_review_not_started_yet(self):
+        """If the review is not started yet, the doc does not appear in list."""
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.user,
+                'approver': self.other_user,
+            }
+        )
+        self.assertFalse(doc.latest_revision.is_under_review())
+        res = self.client.get(self.url)
+        self.assertNotContains(res, '<td class="columndocument_key"')
+
+    def test_previous_step(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.user,
+                'approver': self.other_user,
+            }
+        )
+        doc.latest_revision.start_review()
+        res = self.client.get(self.url)
+        self.assertContains(res, '<td class="columndocument_key"')
+
+    def test_current_step(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.user,
+                'approver': self.other_user,
+            }
+        )
+        doc.latest_revision.start_review()
+        doc.latest_revision.end_reviewers_step()
+        res = self.client.get(self.url)
+        self.assertContains(res, '<td class="columndocument_key"')
+
+    def test_step_finished(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.user,
+                'approver': self.other_user,
+            }
+        )
+        doc.latest_revision.start_review()
+        doc.latest_revision.end_leader_step()
+        res = self.client.get(self.url)
+        self.assertNotContains(res, '<td class="columndocument_key"')
+
+
+class ApproverDocumentListTests(TestCase):
+
+    def setUp(self):
+        self.category = CategoryFactory()
+        self.user = UserFactory(
+            email='testadmin@phase.fr',
+            password='pass',
+            is_superuser=True,
+            category=self.category
+        )
+        self.other_user = UserFactory(
+            email='test@phase.fr',
+            category=self.category
+        )
+        self.client.login(email=self.user.email, password='pass')
+        self.url = reverse('approver_review_document_list')
+
+    def test_previous_step(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.other_user,
+                'approver': self.user,
+            }
+        )
+        doc.latest_revision.start_review()
+        res = self.client.get(self.url)
+        self.assertContains(res, '<td class="columndocument_key"')
+
+        doc.latest_revision.end_reviewers_step()
+        res = self.client.get(self.url)
+        self.assertContains(res, '<td class="columndocument_key"')
+
+    def test_current_step(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.other_user,
+                'approver': self.user,
+            }
+        )
+        doc.latest_revision.start_review()
+        doc.latest_revision.end_leader_step()
+        res = self.client.get(self.url)
+        self.assertContains(res, '<td class="columndocument_key"')
+
+    def test_step_finished(self):
+        doc = DocumentFactory(
+            revision={
+                'reviewers': [self.other_user],
+                'leader': self.other_user,
+                'approver': self.user,
+            }
+        )
+        doc.latest_revision.start_review()
+        doc.latest_revision.end_review()
         res = self.client.get(self.url)
         self.assertNotContains(res, '<td class="columndocument_key"')
--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,24 +1,27 @@
 from django.views.generic import ListView
-from django.db.models import Q
 
 from accounts.views import LoginRequiredMixin
 from documents.utils import get_all_revision_classes
 from reviews.models import ReviewMixin
 
 
-class ReviewersDocumentList(LoginRequiredMixin, ListView):
+class BaseReviewDocumentList(LoginRequiredMixin, ListView):
     template_name = 'reviews/document_list.html'
     context_object_name = 'revisions'
 
     def get_context_data(self, **kwargs):
-        context = super(ReviewersDocumentList, self).get_context_data(**kwargs)
+        context = super(BaseReviewDocumentList, self).get_context_data(**kwargs)
         context.update({
             'reviews_active': True,
         })
         return context
 
+    def step_filter(self, qs):
+        """Filter document list to get reviews at the current step."""
+        raise NotImplementedError('Implement me in the child class.')
+
     def get_queryset(self):
-        """Get all documents under review.
+        """Base queryset to fetch all documents under review.
 
         Since documents can be of differente types, we need to launch a query
         for every document type that is reviewable.  We assume that the number
@@ -26,28 +29,40 @@ class ReviewersDocumentList(LoginRequiredMixin, ListView):
         anything to optimize performances for now.
 
         """
-        user = self.request.user
-        q_reviewers = Q(reviewers=user)
-        q_approver = Q(approver=user)
-        q_leader = Q(leader=user)
-
         revisions = []
         klasses = [klass for klass in get_all_revision_classes() if issubclass(klass, ReviewMixin)]
 
         for klass in klasses:
             qs = klass.objects \
-                .filter(q_reviewers | q_approver | q_leader) \
                 .exclude(review_start_date=None) \
                 .filter(review_end_date=None) \
                 .select_related('document')
+            qs = self.step_filter(qs)
             revisions += list(qs)
 
         return revisions
 
 
-class LeaderDocumentList(LoginRequiredMixin, ListView):
-    pass
+class ReviewersDocumentList(BaseReviewDocumentList):
+    """Display the list of documents at the first review step."""
+
+    def step_filter(self, qs):
+        return qs \
+            .filter(reviewers_step_closed=None) \
+            .filter(reviewers=self.request.user)
 
 
-class ApproverDocumentList(LoginRequiredMixin, ListView):
-    pass
+class LeaderDocumentList(BaseReviewDocumentList):
+    """Display the list of documents at the two first review steps."""
+
+    def step_filter(self, qs):
+        return qs \
+            .filter(leader_step_closed=None) \
+            .filter(leader=self.request.user)
+
+
+class ApproverDocumentList(BaseReviewDocumentList):
+    """Display the list of documents at the three review steps."""
+
+    def step_filter(self, qs):
+        return qs.filter(approver=self.request.user)
