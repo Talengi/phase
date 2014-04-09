2ccbfb63d14f1a9b243ab24161c8e0560d7fb175
Thibault Jouannic <thibault@miximum.fr>
2014-04-09 09:18:51 +0200

    Init reviewer list

---
+++ src/reviews/templatetags/review_form.py
@@ -0,0 +1,40 @@
+from django import template
+
+
+register = template.Library()
+
+
+def icon(name):
+    return '<span class="glyphicon glyphicon-%s"></span>' % name
+
+
+def icons(names):
+    return ' '.join(icon(name) for name in names)
+
+
+@register.simple_tag
+def review_leader_icons(revision):
+    names = []
+    if revision.leader_step_closed:
+        names.append('ok')
+    else:
+        names.append('remove')
+
+    if revision.leader_comments:
+        names.append('comment')
+
+    return icons(names)
+
+
+@register.simple_tag
+def review_approver_icons(revision):
+    names = []
+    if revision.review_end_date:
+        names.append('ok')
+    else:
+        names.append('remove')
+
+    if revision.approver_comments:
+        names.append('comment')
+
+    return icons(names)
