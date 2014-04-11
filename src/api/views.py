39b59567ef3f3a9546eb032c405bcf2a5f691afa
Thibault Jouannic <thibault@miximum.fr>
2014-04-11 10:57:07 +0200

    Filter search user queryset

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 
 from accounts.models import User
@@ -7,6 +8,15 @@ from api.serializers import UserSerializer
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     model = User
     serializer_class = UserSerializer
+    paginate_by_param = 'page_limit'
 
     def get_queryset(self):
-        return User.objects.all()
+        qs = User.objects.all()
+
+        q = self.request.QUERY_PARAMS.get('q', None)
+        if q:
+            q_name = Q(name__icontains=q)
+            q_email = Q(email__icontains=q)
+            qs = qs.filter(q_name | q_email)
+
+        return qs
