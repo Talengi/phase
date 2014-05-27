33fe194cb0bff0bedde212654f449a00c46bf3b1
Thibault Jouannic <thibault@miximum.fr>
2014-05-27 09:50:51 +0200

    Import action breadcrumb

--- src/imports/views.py
+++ src/imports/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import CreateView
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
 
 from imports.forms import FileUploadForm
 
@@ -6,3 +8,6 @@ from imports.forms import FileUploadForm
 class FileUpload(CreateView):
     template_name = 'imports/upload_file.html'
     form_class = FileUploadForm
+
+    def breadcrumb_section(self):
+        return _('Import file'), reverse('import')
