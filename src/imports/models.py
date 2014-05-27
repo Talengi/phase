5bbfbcd803658009afb8070ce751b02d10ddf8f8
Thibault Jouannic <thibault@miximum.fr>
2014-05-27 11:04:17 +0200

    __str__ and url method for import batch object

--- src/imports/models.py
+++ src/imports/models.py
@@ -1,12 +1,17 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
 from django_extensions.db.fields import UUIDField
 from model_utils import Choices
 
 from documents.models import Document
 
 
+@python_2_unicode_compatible
 class ImportBatch(models.Model):
     STATUSES = Choices(
         ('new', _('New')),
@@ -35,6 +40,12 @@ class ImportBatch(models.Model):
     class Meta:
         verbose_name = _('Import batch')
         verbose_name_plural = _('Import batches')
+
+    def __str__(self):
+        return 'Import {} ({})'.format(self.uid, self.imported_type)
+
+    def get_absolute_url(self):
+        return reverse('import_status', args=[self.uid])
 
 
 class Import(models.Model):
