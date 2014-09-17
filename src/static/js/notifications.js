ed78f33bd1ed23cb68d011fdbd865fa82da78d6b
Thibault Jouannic <thibault@miximum.fr>
2014-09-17 09:38:13 +0200

    Only show notif button when there are notifs to read

--- src/static/js/notifications.js
+++ src/static/js/notifications.js
@@ -2,7 +2,17 @@ jQuery(function($) {
     "use strict";
 
     var notifButton = $('#notifications-button');
+    var notifList = $('#notifications div.notification');
+
+    // TODO Get this magic value from python instead
     var markAsReadUrl = '/api/notifications/mark_as_read/';
+
+    // activate the button if there are existing notifications
+    // it could be done in the template, but that would require
+    // an additional query to count the notification number
+    if(notifList.length > 0) {
+        notifButton.removeClass('disabled');
+    }
 
     notifButton.one('click', function() {
         $.post(markAsReadUrl, {})
