0711a0cc06c2a0a13021712da03067e41c8fe6d2
Thibault Jouannic <thibault@miximum.fr>
2014-09-17 11:48:48 +0200

    Start / stop review buttons

--- src/documents/tests/test_templates.py
+++ src/documents/tests/test_templates.py
@@ -6,11 +6,11 @@ from categories.factories import CategoryFactory
 from documents.factories import DocumentFactory
 
 
-enabled_review_button = '<button class="btn btn-warning" type="submit">Start review</button>'
-disabled_review_button = '<button class="btn btn-warning disabled pull-right" type="button">Start review</button>'
+review_button = '<button class="btn btn-warning" type="submit">Start review</button>'
 
 
 class DocumentDetailTests(TestCase):
+    """Test buttons presence for an admin / doc controller"""
     def setUp(self):
         self.category = CategoryFactory()
         self.user = UserFactory(
@@ -33,17 +33,58 @@ class DocumentDetailTests(TestCase):
             self.doc.document_key
         ])
 
-    def test_start_review_button_is_preset(self):
+    def test_review_not_started(self):
         res = self.client.get(self.url)
-        self.assertContains(res, enabled_review_button, html=True)
+        self.assertContains(res, review_button, html=True)
+        self.assertNotContains(res, 'Cancel review')
 
-    def test_start_review_button_is_disabled_when_review_starts(self):
+    def test_review_started(self):
         self.doc.latest_revision.start_review()
         res = self.client.get(self.url)
-        self.assertContains(res, disabled_review_button, html=True)
+        self.assertNotContains(res, review_button, html=True)
+        self.assertContains(res, 'Cancel review')
 
-    def test_start_review_button_comes_back_when_review_is_canceled(self):
+    def test_review_canceled(self):
         self.doc.latest_revision.start_review()
         self.doc.latest_revision.cancel_review()
         res = self.client.get(self.url)
-        self.assertContains(res, enabled_review_button, html=True)
+        self.assertContains(res, review_button, html=True)
+        self.assertNotContains(res, 'Cancel review')
+
+
+class SimpleUserDocumentDetailTests(TestCase):
+    """Simple users cannot start reviews."""
+
+    def setUp(self):
+        self.category = CategoryFactory()
+        self.user = UserFactory(
+            name='User',
+            password='pass',
+            is_superuser=False,
+            category=self.category)
+        self.client.login(username=self.user.email, password='pass')
+        self.doc = DocumentFactory(
+            category=self.category,
+            revision={
+                'reviewers': [self.user],
+                'leader': self.user,
+                'approver': self.user,
+            }
+        )
+        self.url = reverse("document_detail", args=[
+            self.category.organisation.slug,
+            self.category.slug,
+            self.doc.document_key
+        ])
+
+    def test_start_review_button_is_present(self):
+        res = self.client.get(self.url)
+        self.assertNotContains(res, review_button, html=True)
+
+    def test_cancel_review_button(self):
+        res = self.client.get(self.url)
+        self.assertNotContains(res, 'Cancel review')
+
+        self.doc.latest_revision.start_review()
+        res = self.client.get(self.url)
+        self.assertNotContains(res, 'Cancel review')
