5132ea0828473974ed1524da98aad8e6c315f586
Thibault Jouannic <thibault@miximum.fr>
2014-10-01 09:39:33 +0200

    Get a deleted by mistyake function back

--- src/documents/tests/casper_tests/tests.js
+++ src/documents/tests/casper_tests/tests.js
@@ -1,10 +1,24 @@
 var casper = casper || {};
-var utils = require('utils');
 var document_list_url = casper.cli.options['url'];
-
 
 casper.options.clientScripts.push("../../../static/js/vendor/sinon.js");
 
+function inject_cookies() {
+    var m = casper.cli.options['url-base'].match(/https?:\/\/([^:]+)(:\d+)?\//);
+    var domain = m ? m[1] : 'localhost';
+
+    for (var key in casper.cli.options) {
+        if (key.indexOf('cookie-') === 0) {
+            var cn = key.substring('cookie-'.length);
+            var c = phantom.addCookie({
+                name: cn,
+                value: casper.cli.options[key],
+                domain: domain
+            });
+        }
+    }
+}
+inject_cookies();
 
 casper.test.begin('Document list tests', 0, function suite(test) {
     casper.start(document_list_url, function() {
