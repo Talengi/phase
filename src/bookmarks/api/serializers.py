a1ba35cae913f06e120f310fc5facbbc4e1b032c
Thibault Jouannic <thibault@miximum.fr>
2014-10-24 11:19:46 +0200

    Initial bookmark api

---
+++ src/bookmarks/api/serializers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+
+from bookmarks.models import Bookmark
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    user = serializers.Field(source='user.email')
+
+    class Meta:
+        model = Bookmark
+        fields = ('id', 'user', 'url')
