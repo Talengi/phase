1d9c07e21ff36fdde586a8aa885e67035a9c5ba7
Thibault Jouannic <thibault@miximum.fr>
2014-10-28 11:12:33 +0100

    Querystring model

---
+++ src/static/js/querystring.js
@@ -0,0 +1,43 @@
+var Phase = Phase || {};
+
+(function(exports, Phase, Backbone, _) {
+    "use strict";
+
+    Phase.Models = Phase.Models || {};
+
+    /**
+     * Build an object from a querystring
+     */
+    Phase.Models.Querystring = Backbone.Model.extend({
+        constructor: function(querystring) {
+            querystring = querystring || window.location.search;
+
+            // stript everything before '?'
+            var startIndex = querystring.indexOf('?');
+            if (startIndex !== 0) {
+                querystring = querystring.substring(startIndex + 1);
+            }
+
+            var attrs = this.parse(querystring);
+            Backbone.Model.apply(this, [attrs]);
+        },
+        /**
+         * Converts the querystring into an object.
+         *
+         * The `querystring` must be the part after the "?" in the url
+         */
+        parse: function(querystring) {
+            var data = {};
+            var params = querystring.split('&');
+            for (var i = 0; i < params.length; i++) {
+                var param = params[i].split('=');
+                var key = decodeURIComponent(param[0]);
+                var value = decodeURIComponent(param[1]);
+                data[key] = value;
+            }
+
+            return data;
+        }
+    });
+
+})(this, Phase, Backbone, _);
