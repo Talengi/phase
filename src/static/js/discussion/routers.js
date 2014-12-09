f0e0bdefd8f19a03e560e90cb8988914affa6213
Thibault Jouannic <thibault@miximum.fr>
2014-12-09 11:29:56 +0100

    Initialize review js app structure

---
+++ src/static/js/discussion/routers.js
@@ -0,0 +1,20 @@
+var Phase = Phase || {};
+
+(function(exports, Phase, Backbone, _) {
+    "use strict";
+
+    var dispatcher = Phase.Events.dispatcher;
+
+    Phase.Routers = {};
+
+    /**
+     * A single route router to initialize the different models and views.
+     **/
+    Phase.Routers.ReviewRouter = Backbone.Router.extend({
+        routes: {
+            '': 'reviewForm'
+        },
+        reviewForm: function() {
+        }
+    });
+})(this, Phase, Backbone, _);
