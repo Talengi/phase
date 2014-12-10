2312276b6025d293bf81133a18588d94dcb4051d
Thibault Jouannic <thibault@miximum.fr>
2014-12-10 14:42:51 +0100

    Specific fields for author id and email

--- src/discussion/api/serializers.py
+++ src/discussion/api/serializers.py
@@ -10,15 +10,14 @@ from discussion.models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
     document = serializers.Field()
-    author = serializers.Field(source='author.email')
+    author = serializers.Field(source='author_id')
+    author_email = serializers.Field(source='author.email')
     created_on = serializers.DateTimeField(read_only=True)
     formatted_created_on = serializers.DateTimeField(read_only=True, source='created_on')
     formatted_body = serializers.Field(source='body')
 
     class Meta:
         model = Note
-        fields = ('id', 'document', 'author', 'body', 'formatted_body',
-                  'created_on', 'formatted_created_on')
 
     def transform_formatted_created_on(self, obj, value):
         formatted = filters.date(value, 'SHORT_DATETIME_FORMAT')
