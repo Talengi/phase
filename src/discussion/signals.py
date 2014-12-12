0fd90555de25466ea7bef9a7900bcab1b6d2364c
Thibault Jouannic <thibault@miximum.fr>
2014-12-12 14:58:12 +0100

    Notify user when a message contains mentions

--- src/discussion/signals.py
+++ src/discussion/signals.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 from reviews.signals import review_canceled
 from discussion.models import Note
@@ -14,3 +15,8 @@ def on_review_canceled(sender, instance, **kwargs):
         .filter(document=instance.document) \
         .filter(revision=instance.revision)
     notes.delete()
+
+
+@receiver(post_save, sender=Note, dispatch_uid='notification_post_save')
+def parse_mentions(sender, instance, **kwargs):
+    instance.notify_mentionned_users()
