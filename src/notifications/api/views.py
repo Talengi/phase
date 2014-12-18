4f42d82472fcb5d853702c13c42a4cabe2ad6f36
Thibault Jouannic <thibault@miximum.fr>
2014-12-18 16:58:18 +0100

    Use pagination settings in notifications api

--- src/notifications/api/views.py
+++ src/notifications/api/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
@@ -16,11 +21,14 @@ class NoCSRFAuthentication(SessionAuthentication):
 class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
     model = Notification
     serializer_class = NotificationSerializer
+    paginate_by = settings.API_PAGINATE_BY
     paginate_by_param = 'page_limit'
     authentication_classes = (NoCSRFAuthentication,)
 
     def get_queryset(self):
-        return Notification.objects.filter(user=self.request.user)
+        return Notification.objects \
+            .filter(user=self.request.user) \
+            .order_by('-created_on')
 
     @list_route(methods=['post'])
     def mark_as_read(self, request):
