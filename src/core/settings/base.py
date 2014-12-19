85723ba2acfe8eb88c7f752a66951ff2603f1df1
Thibault Jouannic <thibault@miximum.fr>
2014-12-19 10:52:12 +0100

    Disable incompatible debug-toolbar panel

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -481,3 +481,18 @@ USE_X_SENDFILE = False
 IMPORT_ROOT = SITE_ROOT.child('import')
 
 # ######### END CUSTOM CONFIGURATION
+
+# Debug toolbar DJ 1.7 compatibility problems
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+    # 'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+]
