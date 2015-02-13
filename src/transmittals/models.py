9ed3d08e85835cf9210b4525e68b4ee7f134c30c
Thibault Jouannic <thibault@miximum.fr>
2015-02-13 11:58:16 +0100

    Create the trs import model

--- src/transmittals/models.py
+++ src/transmittals/models.py
@@ -8,6 +8,8 @@ from django.utils import timezone
 
 from model_utils import Choices
 
+from documents.models import Document
+from documents.reviews import CLASSES
 from metadata.fields import ConfigurableChoiceField
 
 
@@ -55,3 +57,59 @@ class Transmittal(models.Model):
             self.originator,
             self.recipient,
             self.sequential_number)
+
+
+class TrsRevision(models.Model):
+    """Stores data imported from a single line in the csv."""
+    STATUSES = Choices(
+        'new',
+        'accepted',
+        'refused',
+    )
+
+    document = models.ForeignKey(
+        Document,
+        verbose_name=_('Document'))
+    title = models.TextField(
+        verbose_name=_('Title'))
+    revision = models.PositiveIntegerField(
+        verbose_name=_('Revision'),
+        default=1)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUSES,
+        default=STATUSES.new)
+    is_new_revision = models.BooleanField(
+        _('Is new revision?'))
+
+    # Those are fields that will one day be configurable
+    # but are static for now.
+    revision_status = ConfigurableChoiceField(
+        verbose_name=_('Status'),
+        default='STD',
+        max_length=3,
+        list_index='STATUSES',
+        null=True, blank=True)
+    docclass = models.IntegerField(
+        verbose_name=_('Class'),
+        default=1,
+        choices=CLASSES)
+    unit = ConfigurableChoiceField(
+        verbose_name=_('Unit'),
+        default='000',
+        max_length=3,
+        list_index='UNITS')
+    discipline = ConfigurableChoiceField(
+        verbose_name=_('Discipline'),
+        default='PCS',
+        max_length=3,
+        list_index='DISCIPLINES')
+    document_type = ConfigurableChoiceField(
+        verbose_name=_('Document Type'),
+        default='PID',
+        max_length=3,
+        list_index='DOCUMENT_TYPES')
+
+    class Meta:
+        verbose_name = _('Trs Revision')
+        verbose_name_plural = _('Trs Revisions')
