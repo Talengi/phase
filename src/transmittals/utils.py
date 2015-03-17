a12585c5a1262eeb34f9159c976a8644e883f0e3
Thibault Jouannic <thibault@miximum.fr>
2015-03-17 11:20:31 +0100

    Add doc to utilitary class

--- src/transmittals/utils.py
+++ src/transmittals/utils.py
@@ -4,7 +4,19 @@ from __future__ import unicode_literals
 
 
 class FieldWrapper(object):
-    """Utility class to access fields scattered across multiple objects."""
+    """Utility class to access fields scattered across multiple objects.
+
+    >>> o1 = {'var1': 'toto', 'var2': 'tata'}
+    >>> o2 = {'var3': 'riri', 'var4': 'fifi'}
+    >>> wrapped = FieldWrapper((o1, o2))
+    >>> wrapped.var1
+    'toto'
+    >>> wrapped.var3
+    'riri
+    >>> wrapped['var4']
+    'fifi'
+
+    """
 
     def __init__(self, objects):
         self.objects = objects
