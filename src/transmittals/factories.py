a1d1f538b944ad006f78ad8e1bbb7fbbd108efba
Thibault Jouannic <thibault@miximum.fr>
2015-03-26 16:15:22 +0100

    Link transmittal to document in TransmittalFactory

--- src/transmittals/factories.py
+++ src/transmittals/factories.py
@@ -4,7 +4,12 @@ from __future__ import unicode_literals
 
 import factory
 
+from documents.factories import DocumentFactory
 from transmittals.models import Transmittal, TransmittalRevision, TrsRevision
+
+
+class TransmittalRevisionFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = TransmittalRevision
 
 
 class TransmittalFactory(factory.DjangoModelFactory):
@@ -18,10 +23,12 @@ class TransmittalFactory(factory.DjangoModelFactory):
     originator = 'CTR'
     recipient = 'CLT'
     sequential_number = factory.Sequence(lambda n: n + 1)
-
-
-class TransmittalRevisionFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = TransmittalRevision
+    document = factory.SubFactory(
+        DocumentFactory,
+        document_key=factory.SelfAttribute('..document_key'))
+    latest_revision = factory.SubFactory(
+        TransmittalRevisionFactory,
+        document=factory.SelfAttribute('..document'))
 
 
 class TrsRevisionFactory(factory.DjangoModelFactory):
