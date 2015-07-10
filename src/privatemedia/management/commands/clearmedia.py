183d12ab9575947568f0350e5733c1c6989f9c60
Thibault Jouannic <thibault@miximum.fr>
2015-07-10 16:51:58 +0200

    Clearmedia doc

--- src/privatemedia/management/commands/clearmedia.py
+++ src/privatemedia/management/commands/clearmedia.py
@@ -16,6 +16,19 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    """Cleanup private storage.
+
+    Since Django 1.3, FileFields instances are not automaticaly deleted upon's
+    the mode deletion anymore.
+
+    This is to preserve data integrity in case of transactions rollbacks.
+
+    The drawback is that cleaning file is our responsability.
+
+    This tasks cleans the private storage directory by removing all files that
+    are not present in db anymore.
+
+    """
     def handle(self, *args, **options):
         logger.info('Starting private media files cleanup')
 
