44316a1aa3b7687dcc4deda4fbfc571364b1d12a
Thibault Jouannic <thibault@miximum.fr>
2015-07-17 14:38:28 +0200

    Replace prints with logs

--- src/search/management/commands/delete_index.py
+++ src/search/management/commands/delete_index.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
@@ -10,9 +12,12 @@ from elasticsearch.exceptions import ConnectionError
 from search.utils import delete_index
 
 
+logger = logging.getLogger(__name__)
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Deleting index %s' % settings.ELASTIC_INDEX)
+        logger.info('Deleting index %s' % settings.ELASTIC_INDEX)
 
         try:
             delete_index()
