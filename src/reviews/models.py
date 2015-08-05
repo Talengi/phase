acbff2f01a5248979b44df525229f012bd65b9cc
Thibault Jouannic <thibault@miximum.fr>
2015-08-05 10:26:16 +0200

    Review post-import hook

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import datetime
 
 from django.db import models, transaction
@@ -227,9 +230,9 @@ class ReviewMixin(models.Model):
         review_canceled.send(sender=self.__class__, instance=self)
 
     @transaction.atomic
-    def end_reviewers_step(self, save=True):
+    def end_reviewers_step(self, save=True, at_date=None):
         """Ends the first step of the review."""
-        self.reviewers_step_closed = datetime.date.today()
+        self.reviewers_step_closed = at_date or datetime.date.today()
 
         Review.objects \
             .filter(document=self.document) \
@@ -241,16 +244,18 @@ class ReviewMixin(models.Model):
             self.save(update_document=True)
 
     @transaction.atomic
-    def end_leader_step(self, save=True):
+    def end_leader_step(self, save=True, at_date=None):
         """Ends the second step of the review.
 
         Also ends the first step if it wasn't already done.
 
         """
         if self.reviewers_step_closed is None:
-            self.end_reviewers_step(save=False)
+            self.end_reviewers_step(save=False, at_date=at_date)
 
-        self.leader_step_closed = datetime.date.today()
+        end_date = at_date or datetime.date.today()
+
+        self.leader_step_closed = end_date
 
         Review.objects \
             .filter(document=self.document) \
@@ -259,7 +264,7 @@ class ReviewMixin(models.Model):
             .update(closed=True)
 
         if not self.approver_id:
-            self.review_end_date = datetime.date.today()
+            self.review_end_date = end_date
 
         if save:
             self.save(update_document=True)
@@ -279,16 +284,16 @@ class ReviewMixin(models.Model):
             self.save(update_document=True)
 
     @transaction.atomic
-    def end_review(self, save=True):
+    def end_review(self, save=True, at_date=None):
         """Ends the review.
 
         Also ends the steps before.
 
         """
         if self.leader_step_closed is None:
-            self.end_leader_step(save=False)
+            self.end_leader_step(save=False, at_date=at_date)
 
-        self.review_end_date = datetime.date.today()
+        self.review_end_date = at_date or datetime.date.today()
 
         Review.objects \
             .filter(document=self.document) \
@@ -370,8 +375,40 @@ class ReviewMixin(models.Model):
     def post_trs_import(self, trs_revision):
         """See `documents.models.MetadataRevision.post_trs_import`
 
+        If we are importing a revision with review data, we need to make sure
+        Phase objects are left in a consistent state.
+
         We need to create `Review` objects if the leader and approver review
         data is set in the trs_revision object.
 
         """
-        pass
+        category = self.document.category
+        user_qs = User.objects.filter(categories=category)
+
+        # Is there a defined leader?
+        if trs_revision.review_leader:
+            self.leader = user_qs.get(name=trs_revision.review_leader)
+
+        # Is there a defined approver
+        if trs_revision.review_approver:
+            self.approver = user_qs.get(name=trs_revision.review_approver)
+
+        # Was the review started?
+        if trs_revision.review_start_date:
+            self.start_review(
+                at_date=trs_revision.review_start_date,
+                due_date=trs_revision.review_due_date)
+
+        # Did the leader already submit a comment?
+        if trs_revision.leader_comment_date:
+            self.end_leader_step(
+                at_date=trs_revision.leader_comment_date,
+                save=False)
+
+        # Is there an approver and did he submit a comment?
+        if self.approver_id and trs_revision.approver_comment_date:
+            self.end_review(
+                at_date=trs_revision.approver_comment_date,
+                save=False)
+
+        self.save()
