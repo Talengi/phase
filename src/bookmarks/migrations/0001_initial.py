648d1243f5d7813cf76be2c12315c329b0b33363
Thibault Jouannic <thibault@miximum.fr>
2015-08-11 14:31:28 +0200

    Generate Initial migrations

---
+++ src/bookmarks/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Bookmark',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='Name')),
+                ('url', models.CharField(max_length=200, verbose_name='Url')),
+                ('created_on', models.DateField(default=django.utils.timezone.now, verbose_name='Created on')),
+            ],
+            options={
+                'verbose_name': 'Bookmark',
+                'verbose_name_plural': 'Bookmarks',
+            },
+        ),
+    ]
---
+++ src/categories/migrations/0001_initial.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        ('auth', '0006_require_contenttypes_0002'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+                'verbose_name': 'Category',
+                'verbose_name_plural': 'Categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='CategoryTemplate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='Name')),
+                ('slug', models.SlugField(verbose_name='Slug')),
+                ('description', models.CharField(max_length=200, null=True, verbose_name='Description', blank=True)),
+                ('metadata_model', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'verbose_name': 'Category template',
+                'verbose_name_plural': 'Category templates',
+            },
+        ),
+        migrations.CreateModel(
+            name='Organisation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='Name')),
+                ('slug', models.SlugField(verbose_name='Slug')),
+                ('description', models.CharField(max_length=200, null=True, verbose_name='Description', blank=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='category_template',
+            field=models.ForeignKey(verbose_name='Category template', to='categories.CategoryTemplate'),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='groups',
+            field=models.ManyToManyField(to='auth.Group', blank=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='organisation',
+            field=models.ForeignKey(related_name='categories', verbose_name='Organisation', to='categories.Organisation'),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='users',
+            field=models.ManyToManyField(related_name='categories', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='category',
+            unique_together=set([('category_template', 'organisation')]),
+        ),
+    ]
