401c469b306b4796a3953436daaf65ea6edbd6d0
Thibault Jouannic <thibault@miximum.fr>
2015-08-11 11:32:44 +0200

    Keep debug toolbar default conf

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -482,20 +482,5 @@ IMPORT_ROOT = SITE_ROOT.child('import')
 
 # ######### END CUSTOM CONFIGURATION
 
-# Debug toolbar DJ 1.7 compatibility problems
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.cache.CacheDebugPanel',
-    # 'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
-]
-
 # Disable a DJ1.7 warning
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
