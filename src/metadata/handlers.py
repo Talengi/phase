02a89df6fb41ec7a16c78a4c0581ddaa913e5c13
Thibault Jouannic <thibault@miximum.fr>
2015-08-13 12:45:43 +0200

    Fix metadata tests

--- src/metadata/handlers.py
+++ src/metadata/handlers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import F, Value as V
+from django.db.models.functions import Concat
 from django.core.cache import cache
 
 from metadata.models import ListEntry
@@ -9,8 +11,9 @@ from metadata.models import ListEntry
 def populate_values_list_cache(**kwargs):
     values = ListEntry.objects \
         .select_related('values_list') \
+        .annotate(display=Concat(F('index'), V(' - '), F('value'))) \
         .order_by('values_list__index') \
-        .values_list('values_list__index', 'index', 'value')
+        .values_list('values_list__index', 'index', 'display')
 
     grouped = {}
     for values_list, index, value in values:
