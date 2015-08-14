1cee21a3c7bb950643711f06403338a0d8542067
Thibault Jouannic <thibault@miximum.fr>
2015-08-14 15:12:37 +0200

    Reload cache task

---
+++ src/metadata/management/commands/reload_metadata_cache.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from metadata.handlers import populate_values_list_cache
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        logger.info('Populating values list cache')
+        populate_values_list_cache()
