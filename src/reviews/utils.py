8461cdf74393ac228b953bfd75764f29ebbdf707
Thibault Jouannic <thibault@miximum.fr>
2015-09-08 10:14:53 +0200

    Always show distribution list
    Even for revisions that were not reviewed.

--- src/reviews/utils.py
+++ src/reviews/utils.py
@@ -17,23 +17,32 @@ def get_cached_reviews(revision):
 
     All the reviews will be fetched in a single query and cached.
 
+    Also, for revision which review was never started, there is no review
+    objects to fetch, so we need to create some dummy ones for display purpose.
+
+    See https://trello.com/c/CdZF9eAG/174-afficher-la-liste-de-distribution-d-un-document
+
     """
-    reviews = get_all_reviews(revision.document_id)
+    reviews = get_all_reviews(revision.document)
     if revision.revision in reviews:
         revision_reviews = reviews[revision.revision]
     else:
-        revision_reviews = []
+        dummy_reviews = get_dummy_reviews(revision)
+        if revision.revision in dummy_reviews:
+            revision_reviews = dummy_reviews[revision.revision]
+        else:
+            revision_reviews = []
     return revision_reviews
 
 
-def get_all_reviews(document_id):
+def get_all_reviews(document):
     """Return a dictionnary of revision indexed reviews."""
-    cache_key = 'all_reviews_{}'.format(document_id)
+    cache_key = 'all_reviews_{}'.format(document.id)
     all_reviews = cache.get(cache_key, None)
 
     if all_reviews is None:
         qs = Review.objects \
-            .filter(document_id=document_id) \
+            .filter(document=document) \
             .order_by('revision', 'id') \
             .select_related('reviewer')
 
@@ -44,3 +53,42 @@ def get_all_reviews(document_id):
         cache.set(cache_key, all_reviews, 60)
 
     return all_reviews
+
+
+def get_dummy_reviews(revision):
+    """Return a dictionary of Review objects for."""
+    cache_key = 'dummy_reviews_{}'.format(revision.document_id)
+    dummy_reviews = cache.get(cache_key, None)
+
+    if dummy_reviews is None:
+
+        revisions = revision.__class__.objects \
+            .filter(document=revision.document) \
+            .filter(review_start_date=None) \
+            .select_related('leader', 'approver') \
+            .prefetch_related('reviewers')
+
+        dummy_reviews = {}
+        for revision in revisions:
+            revision_reviews = []
+            revision_reviews.append(Review(
+                role='leader',
+                reviewer=revision.leader,
+                document_id=revision.document_id))
+
+            revision_reviews.append(Review(
+                role='approver',
+                reviewer=revision.approver,
+                document_id=revision.document_id))
+
+            for reviewer in revision.reviewers.all():
+                revision_reviews.append(Review(
+                    role='reviewer',
+                    reviewer=reviewer,
+                    document_id=revision.document_id))
+
+            dummy_reviews[revision.revision] = revision_reviews
+
+        cache.set(cache_key, dummy_reviews, 60)
+
+    return dummy_reviews
