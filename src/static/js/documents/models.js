6fb2cabb5e65e09e9111be36c3c34522dddb6369
Thibault Jouannic <thibault@miximum.fr>
2015-09-09 15:52:04 +0200

    Convert js booleans to strings before string comparison

--- src/static/js/documents/models.js
+++ src/static/js/documents/models.js
@@ -23,7 +23,17 @@ var Phase = Phase || {};
                 start: 0,
                 size: Phase.Config.paginateBy
             };
-            return _.extend(defaults, Phase.Config.filterDefaults);
+            var updatedDefault = _.extend(defaults, Phase.Config.filterDefaults);
+
+            // Boolean values must be converted to strings because input
+            // values "true" and "false" are not cast to bools before
+            // comparison
+            return _.mapObject(updatedDefault, function(val, key) {
+                if (typeof(val) == 'boolean') {
+                    val = val.toString();
+                }
+                return val;
+            });
         },
         /**
          * Reset the search parameters to default.
