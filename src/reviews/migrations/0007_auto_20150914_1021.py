6a11969c55c40025afa8623af9a7ff00d3dd0b30
Thibault Jouannic <thibault@miximum.fr>
2015-09-14 10:22:56 +0200

    Create migration to take new review choice into account
    This is a void operation from a db point of view

---
+++ src/reviews/migrations/0007_auto_20150914_1021.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reviews', '0006_auto_20150908_1151'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='review',
+            name='status',
+            field=models.CharField(default='pending', max_length=30, verbose_name='Status', choices=[('void', ''), ('pending', 'Pending'), ('progress', 'In progress'), ('reviewed', 'Reviewed without comments'), ('commented', 'Reviewed with comments'), ('not_reviewed', 'Not reviewed')]),
+        ),
+    ]
