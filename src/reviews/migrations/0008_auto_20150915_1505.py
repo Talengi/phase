456160338a7e91e704e23f0bafc167e6eebc029f
Thibault Jouannic <thibault@miximum.fr>
2015-09-15 15:05:24 +0200

    Migration for new statuses

---
+++ src/reviews/migrations/0008_auto_20150915_1505.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reviews', '0007_auto_20150914_1021'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='review',
+            name='status',
+            field=models.CharField(default='pending', max_length=30, verbose_name='Status', choices=[('void', ''), ('pending', 'Pending'), ('progress', 'In progress'), ('reviewed', 'Reviewed'), ('commented', 'Reviewed'), ('not_reviewed', 'Not reviewed')]),
+        ),
+    ]
