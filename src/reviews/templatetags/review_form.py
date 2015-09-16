46437fdda83842c7121fea6c1ed5fa1994d068ce
Thibault Jouannic <thibault@miximum.fr>
2015-09-16 16:41:13 +0200

    File link: only show basename
    Fixes #130

--- src/reviews/templatetags/review_form.py
+++ src/reviews/templatetags/review_form.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
 from django import template
 from django.utils.translation import ugettext_lazy as _
 from django.template.defaultfilters import filesizeformat
@@ -8,10 +13,12 @@ register = template.Library()
 
 @register.simple_tag
 def file_link(file, name=None):
-    name = name if name else file
+    name = name if name else os.path.basename(file.name)
     if file:
-        link = '<a href="%s">%s</a> (%s)' % (
-            file.url, name, filesizeformat(file.size))
+        link = '<a href="{}">{}</a> ({})'.format(
+            file.url,
+            name,
+            filesizeformat(file.size))
     else:
         link = ''
     return link
