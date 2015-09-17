610ec3b6501b44e62fd8c8017efa61e04dd5ae2e
Thibault Jouannic <thibault@miximum.fr>
2015-09-17 13:44:42 +0200

    Export create and list views

--- src/exports/urls.py
+++ src/exports/urls.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 
-from exports.views import ExportCreate
+from exports.views import ExportCreate, ExportList, DownloadView
 
 
 urlpatterns = patterns(
     '',
 
-    # Favorites
     url(r'^$',
+        ExportList.as_view(),
+        name="export_list"),
+    url(r'^(?P<organisation>[\w-]+)/(?P<category>[\w-]+)/$',
         ExportCreate.as_view(),
         name="export_create"),
+    url(r'^(?P<filename>[-\w]+\.[\w]{3,4})$',
+        DownloadView.as_view(),
+        name='export_download')
 )
