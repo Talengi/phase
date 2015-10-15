5fa4d6a42d8b367fe0d171c61123aba905603926
Thibault Jouannic <thibault@miximum.fr>
2015-10-15 09:55:56 +0200

    Create class CategoryMixin

--- src/categories/views.py
+++ src/categories/views.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.views.generic import ListView
+from django.http import Http404
+
 from braces.views import LoginRequiredMixin
+from annoying.functions import get_object_or_None
 
 from .models import Category
 
@@ -14,3 +20,25 @@ class CategoryList(LoginRequiredMixin, ListView):
             .order_by('organisation__name')
 
         return qs
+
+
+class CategoryMixin(object):
+    """Use this mixin to extract the category in url."""
+
+    def dispatch(self, request, *args, **kwargs):
+        self.extract_category()
+        return super(CategoryMixin, self).dispatch(request, *args, **kwargs)
+
+    def extract_category(self):
+        """Set the `self.category` variable."""
+        organisation_slug = self.kwargs['organisation']
+        category_slug = self.kwargs['category']
+
+        qs = Category.objects \
+            .select_related() \
+            .filter(users=self.request.user) \
+            .filter(organisation__slug=organisation_slug) \
+            .filter(category_template__slug=category_slug)
+        self.category = get_object_or_None(qs)
+        if self.category is None:
+            raise Http404('Category not found')
