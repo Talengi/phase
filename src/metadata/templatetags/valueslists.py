e07247b76d2cab445446425df5ca3ece66eaa812
Thibault Jouannic <thibault@miximum.fr>
2015-11-16 10:35:05 +0100

    Template tag to retrive a values list

---
+++ src/metadata/templatetags/valueslists.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import template
+from django.core.cache import cache
+
+
+register = template.Library()
+
+
+@register.assignment_tag
+def get_values_list(list_index):
+    """Return a values list from cache."""
+    cache_key = 'values_list_{}'.format(list_index)
+    values = cache.get(cache_key)
+    return values
