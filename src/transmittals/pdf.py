8efe7ef5302febf4b49345277661c0eb13a9e2fa
Thibault Jouannic <thibault@miximum.fr>
2015-11-23 14:36:44 +0100

    Use platypus for document formatting

--- src/transmittals/pdf.py
+++ src/transmittals/pdf.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from io import BytesIO
 
-from reportlab.pdfgen import canvas
 from reportlab.lib.colors import black
 from reportlab.lib import colors
 from reportlab.lib.units import cm, mm
@@ -102,7 +101,6 @@ def build_revisions_table(revisions):
 class TransmittalPdf(object):
     def __init__(self, revision):
         self.buff = BytesIO()
-        self.c = canvas.Canvas(self.buff, pagesize=A4)
         self.styles = getSampleStyleSheet()
         self.width, self.height = A4
         self.revision = revision
@@ -113,12 +111,31 @@ class TransmittalPdf(object):
         self.build_document()
 
     def build_document(self):
-        self._draw_title()
-        self._draw_contract_nb_table()
-        self._draw_subtitle()
-        self._draw_trs_meta()
-        self._draw_sender_addressee()
-        self._draw_way_of_transmission()
+        self.doc = SimpleDocTemplate(
+            self.buff,
+            pagesize=A4,
+            leftMargin=13 * mm,
+            rightMargin=13 * mm,
+            topMargin=13 * mm,
+            bottomtMargin=18 * mm)
+
+        story = [
+            Spacer(0, 4 * cm),
+            self.build_subtitle(),
+            Spacer(0, 6 * mm),
+            self.build_trs_meta(),
+            Spacer(0, 6 * mm),
+            self.build_sender_addressee(),
+            Spacer(0, 6 * mm),
+            self.build_way_of_transmission(),
+            Spacer(0, 6 * mm),
+            self.build_revisions_table(),
+        ]
+        self.doc.build(story, onFirstPage=self.build_header)
+
+    def build_header(self, canvas, doc):
+        self.draw_title(canvas)
+        self.draw_contract_nb_table(canvas)
 
     def get_table_style(self):
         style = TableStyle([
@@ -129,23 +146,23 @@ class TransmittalPdf(object):
         ])
         return style
 
-    def _draw_title(self):
+    def draw_title(self, canvas):
         title = self.category.organisation.name
         p = Paragraph(title, self.styles['Title'])
-        p.wrapOn(self.c, 120 * mm, 150 * mm)
-        p.drawOn(self.c, *self.coord(15, 55))
+        p.wrapOn(canvas, 120 * mm, 150 * mm)
+        p.drawOn(canvas, *self.coord(13, 55))
 
-    def _draw_contract_nb_table(self):
+    def draw_contract_nb_table(self, canvas):
         data = [
             ('Contract NB', self.transmittal.contract_number),
             ('Phase', ''),
         ]
         table = Table(data, hAlign='LEFT', colWidths=[25 * mm, 25 * mm])
         table.setStyle(self.get_table_style())
-        table.wrapOn(self.c, 50 * mm, 50 * mm)
-        table.drawOn(self.c, *self.coord(145, 55))
+        table.wrapOn(canvas, 50 * mm, 50 * mm)
+        table.drawOn(canvas, *self.coord(145, 55))
 
-    def _draw_subtitle(self):
+    def build_subtitle(self):
         style = ParagraphStyle(
             name='trs_style',
             parent=self.styles['Heading2'],
@@ -155,29 +172,26 @@ class TransmittalPdf(object):
             textTransform='uppercase'
         )
         p = Paragraph('Transmittal sheet', style)
-        p.wrapOn(self.c, 130 * mm, 20 * mm)
-        p.drawOn(self.c, *self.coord(15, 70))
+        return p
 
-    def _draw_trs_meta(self):
+    def build_trs_meta(self):
         data = [
             ('Transmittal Number', self.document.document_key),
             ('Issue Date', self.document.created_on),
         ]
         table = Table(data, hAlign='LEFT', colWidths=[45 * mm, None])
         table.setStyle(self.get_table_style())
-        table.wrapOn(self.c, self.width, 20 * mm)
-        table.drawOn(self.c, *self.coord(15, 88))
+        return table
 
-    def _draw_sender_addressee(self):
+    def build_sender_addressee(self):
         text = 'Sender: {}<br />Addressee: {}'.format(
             'XXX Sender name',
             'YYY Addressee name'
         )
         p = Paragraph(text, self.styles['Normal'])
-        p.wrapOn(self.c, self.width, self.height)
-        p.drawOn(self.c, *self.coord(15, 105))
+        return p
 
-    def _draw_way_of_transmission(self):
+    def build_way_of_transmission(self):
         data = [
             ('Way of transmission', ''),
             ('EDMS', 'X'),
@@ -192,8 +206,33 @@ class TransmittalPdf(object):
         style.add('ALIGN', (0, 0), (0, 0), 'CENTER')
         style.add('ALIGN', (1, 0), (1, -1), 'CENTER')
         table.setStyle(style)
-        table.wrapOn(self.c, self.width, self.height)
-        table.drawOn(self.c, *self.coord(15, 150))
+        return table
+
+    def build_revisions_table(self):
+        style = styles['BodyText']
+        style.alignment = TA_LEFT
+        style.wordWrap = 'LTR'
+
+        header = (
+            'Document Number',
+            'Title',
+            'Rev.',
+            'Status',
+            'RC')
+        data = [header]
+        for revision in self.revisions:
+            data.append((
+                Paragraph(revision.document.document_key, style),
+                Paragraph(revision.title, style),
+                Paragraph('%s' % revision.revision, style),
+                Paragraph(revision.status, style),
+                Paragraph(revision.return_code, style)))
+        table = Table(
+            data,
+            hAlign='LEFT',
+            colWidths=[45 * mm, 90 * mm, 15 * mm, 15 * mm, 15 * mm])
+        table.setStyle(self.get_table_style())
+        return table
 
     def coord(self, x, y, unit=mm):
         """Helper class to computes pdf coordinary
@@ -206,8 +245,6 @@ class TransmittalPdf(object):
 
     def as_binary(self):
         """Writes the pdf to buffer and returns it as a binary value."""
-        self.c.showPage()
-        self.c.save()
         pdf_binary = self.buff.getvalue()
         self.buff.close()
         return pdf_binary
