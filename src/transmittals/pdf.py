884c7b0018e2b17d05d5375acd78b6eaf83ff83a
Thibault Jouannic <thibault@miximum.fr>
2015-11-24 11:11:37 +0100

    Display pdf pagination

--- src/transmittals/pdf.py
+++ src/transmittals/pdf.py
@@ -6,12 +6,45 @@ from io import BytesIO
 from django.utils import dateformat
 
 from reportlab.lib import colors
+from reportlab.pdfgen import canvas
 from reportlab.lib.units import cm, mm
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.platypus.tables import Table, TableStyle
 from reportlab.lib.enums import TA_CENTER
+
+
+class NumberedCanvas(canvas.Canvas):
+    """Display pagination on bottom of page.
+
+    Shamelessly stolen from here:
+    http://stackoverflow.com/questions/1087495/reportlab-page-x-of-y-numberedcanvas-and-images
+
+    """
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_page_number(self, page_count):
+        # self.setFont("Helvetica", 7)
+        self.drawRightString(
+            200 * mm,
+            10 * mm,
+            "Page %d of %d" % (self._pageNumber, page_count))
 
 
 class TransmittalPdf(object):
@@ -53,7 +86,7 @@ class TransmittalPdf(object):
             self.build_ack_label(),
             self.build_ack_table(),
         ]
-        self.doc.build(story, onFirstPage=self.build_header)
+        self.doc.build(story, onFirstPage=self.build_header, canvasmaker=NumberedCanvas)
 
     def build_header(self, canvas, doc):
         self.draw_title(canvas)
