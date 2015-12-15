a2e850b1fa8983d225f3213e88ff50ae053cad75
Laurent Paoletti <lp@providenz.fr>
2015-12-15 14:19:58 +0100

    Allow sidebar filter fields to be easily ordered
    A new optional PhaseConfig item allows to define
    the order in which filter fields appear in sidebar.
    Chosing this approach avoid to break the application.

--- src/documents/forms/filters.py
+++ src/documents/forms/filters.py
@@ -1,62 +1,64 @@
+from collections import OrderedDict
+
 from django import forms
-from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 
 class BaseDocumentFilterForm(forms.Form):
     """Base form for filtering documents of any type."""
     size = forms.IntegerField(
-        widget=forms.HiddenInput(),
-        required=False,
-        initial=settings.PAGINATE_BY)
+            widget=forms.HiddenInput(),
+            required=False,
+            initial=settings.PAGINATE_BY)
     start = forms.IntegerField(
-        widget=forms.HiddenInput(),
-        required=False,
-        initial=0)
+            widget=forms.HiddenInput(),
+            required=False,
+            initial=0)
     search_terms = forms.CharField(
-        label=u'Search all columns',
-        required=False)
+            label=u'Search all columns',
+            required=False)
 
 
-# Additional fields that could be needed, e.g for filter fiedls that are
+# Additional fields that could be needed, e.g for filter fields that are
 # not model fields.
 # TODO Find a better way to do this.
 additional_filter_fields = {
     'overdue': forms.ChoiceField(
-        choices=(
-            ('', '---------'),
-            ('true', 'Yes'),
-            ('false', 'No')
-        ),
-        required=False,
-        widget=forms.Select,
-        label=_('Overdue'),
+            choices=(
+                ('', '---------'),
+                ('true', 'Yes'),
+                ('false', 'No')
+            ),
+            required=False,
+            widget=forms.Select,
+            label=_('Overdue'),
     ),
     'under_review': forms.ChoiceField(
-        choices=(
-            ('', '---------'),
-            ('true', 'Yes'),
-            ('false', 'No')
-        ),
-        required=False,
-        widget=forms.Select,
-        label=_('Under review'),
+            choices=(
+                ('', '---------'),
+                ('true', 'Yes'),
+                ('false', 'No')
+            ),
+            required=False,
+            widget=forms.Select,
+            label=_('Under review'),
     ),
     'ack_of_receipt': forms.ChoiceField(
-        choices=(
-            ('', '---------'),
-            ('true', 'Yes'),
-            ('false', 'No')
-        ),
-        required=False,
-        widget=forms.Select,
-        label=_('Ack of receipt'),
+            choices=(
+                ('', '---------'),
+                ('true', 'Yes'),
+                ('false', 'No')
+            ),
+            required=False,
+            widget=forms.Select,
+            label=_('Ack of receipt'),
     )
 }
 
 
 def filterform_factory(model):
-    """Dynamicaly create a filter form for the given Metadata model.
+    """Dynamically create a filter form for the given Metadata model.
 
     Filter fields can be either located in the Metadata class, or in the
     corresponding Revision class.
@@ -73,6 +75,10 @@ def filterform_factory(model):
 
     # Add all field filters to form
     filter_fields = config.filter_fields
+
+    # Get the optional `filter_fields_order` in PhaseConfig
+    filter_fields_order = getattr(config, 'filter_fields_order', None)
+
     for field_name in filter_fields:
         if field_name in all_fields:
             f = all_fields[field_name]
@@ -96,13 +102,27 @@ def filterform_factory(model):
         field.required = False
         field.label = filter_config['label']
         field_list.append((field_name, field))
-
-    field_list = dict(field_list)
-    field_list.update({
-        'sort_by': forms.CharField(
-            widget=forms.HiddenInput(),
-            required=False,
-            initial=model._meta.ordering[0])
-    })
+    field_list.append((
+        'sort_by', forms.CharField(
+                widget=forms.HiddenInput(),
+                required=False,
+                initial=model._meta.ordering[0])
+    ))
     class_name = model.__name__ + 'FilterForm'
-    return type(class_name, (BaseDocumentFilterForm,), dict(field_list))
+    field_dict = OrderedDict(field_list)
+
+    form = type(class_name, (BaseDocumentFilterForm,), field_dict)
+
+    if filter_fields_order:
+        # We need to modify `base_fields` to get the wanted order
+        # Trick used in django admin:
+        # https://github.com/django/django/blob/1.8/django/contrib/auth/forms.py#L318
+
+        # These hidden fields belong to `BaseDocumentFilterForm`.
+        # We have to add them to the `base_fields` by appending them to the
+        # `filter_fields_order` list manually.
+        # TODO Find a better way to do this.
+        filter_fields_order = ['size', 'start'] + filter_fields_order
+        form.base_fields = OrderedDict(
+                (k, form.base_fields[k]) for k in filter_fields_order )
+    return form
