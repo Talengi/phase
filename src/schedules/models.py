2fac511a1c4bf893d534812f1febcffdcc5f6c79
Thibault Jouannic <thibault@miximum.fr>
2016-01-07 10:19:34 +0100

    Add comment to empty mixin class

--- src/schedules/models.py
+++ src/schedules/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 
 class ScheduleMixin(object):
+    """Add behavior of a document with a "Schedule" section.
+
+    This is use to filter document classes in signal handling.
+
+    """
     pass
