2bd8afae37e787162a4b194f9c1636d897eec802
Thibault Jouannic <thibault@miximum.fr>
2016-01-19 14:34:44 +0100

    Fix js url handling regression

--- src/static/js/documents/app.js
+++ src/static/js/documents/app.js
@@ -3,6 +3,7 @@ var Phase = Phase || {};
 jQuery(function($) {
     var router = new Phase.Routers.DocumentListRouter();
     Backbone.history.start({
-        root: Phase.Config.currentUrl
+        root: Phase.Config.currentUrl,
+        pushState: true
     });
 });
