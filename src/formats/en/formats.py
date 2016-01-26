bc4e4030e664eecd6ed43dbf5c164b6929bceaec
Thibault Jouannic <thibault@miximum.fr>
2016-01-26 09:29:13 +0100

    Add comment

--- src/formats/en/formats.py
+++ src/formats/en/formats.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# WARNING
+# If you override this value, you also need to update
+# data-date-format values in
+# templates/documents/form_revisions.html
 SHORT_DATE_FORMAT = 'Y-m-d'
