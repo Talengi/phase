b62f7641165405e5b3e9b7b24033d760a3d5ae3c
Thibault Jouannic <thibault@miximum.fr>
2016-01-26 09:27:29 +0100

    Create custom SHORT_DATE_FORMAT

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -81,6 +81,7 @@ USE_I18N = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
 USE_L10N = True
+FORMAT_MODULE_PATH = 'formats'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
 USE_TZ = True
---
+++ src/formats/en/formats.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+SHORT_DATE_FORMAT = 'Y-m-d'
