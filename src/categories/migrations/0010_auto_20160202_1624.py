55ea0f817bfd82683f008ee458ca4d345f03f3c8
Laurent Paoletti <lp@providenz.fr>
2016-02-02 16:46:51 +0100

    Migrate data from values list to new Contract model

---
+++ src/categories/migrations/0010_auto_20160202_1624.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def copy_contracts_from_values_list(apps, schema_editor):
+    ValuesList = apps.get_model("metadata", "ValuesList")
+    Contract = apps.get_model("categories", "Contract")
+    value_list = ValuesList.objects.filter(index='CONTRACT_NBS').get()
+    for contract in value_list.values.all():
+        Contract.objects.create(number=contract.index, name=contract.value)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('metadata', '0002_return_code_values'),
+        ('categories', '0009_contract'),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_contracts_from_values_list)
+    ]
