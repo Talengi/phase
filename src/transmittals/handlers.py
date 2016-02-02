c9b6a96afbaec78d6ecf0367b816b5d4cc4290d7
Thibault Jouannic <thibault@miximum.fr>
2016-02-02 10:23:42 +0100

    Emit new signal

--- src/transmittals/handlers.py
+++ src/transmittals/handlers.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from transmittals import signals
+
 
 def transmittal_post_save(document, metadata, revision, **kwargs):
     """When metadata is saved, generate the revision pdf."""
     if not revision.pdf_file:
         pdf_file = revision.generate_pdf_file()
         revision.pdf_file.save('thiswillbediscarded.pdf', pdf_file)
+
+        signals.transmittal_pdf_generated.send(
+            document=document,
+            metadata=metadata,
+            revision=revision,
+            sender=metadata.__class__)
