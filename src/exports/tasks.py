122712e40c8e26464dd70e461477f7a32354368f
Laurent Paoletti <lp@providenz.fr>
2016-02-02 09:37:12 +0100

    Remove unused imports

--- src/exports/tasks.py
+++ src/exports/tasks.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 from core.celery import app
-from notifications.models import notify
 
 
 @app.task
