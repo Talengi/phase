f32b557407b448ae96bed812226ac362c88ab569
Thibault Jouannic <thibault@miximum.fr>
2016-02-08 11:19:00 +0100

    Better revision clearable file input template

--- src/documents/widgets.py
+++ src/documents/widgets.py
@@ -6,11 +6,13 @@ from django.forms.widgets import ClearableFileInput
 
 class RevisionClearableFileInput(ClearableFileInput):
     template_with_initial = (
-        '<a href="%(initial_url)s">%(initial)s</a><br />'
-        '%(clear_template)s<br />%(input_text)s: %(input)s'
-    )
-    template_with_clear = (
-        '<div class="clear">%(clear)s '
-        '<label for="%(clear_checkbox_id)s">%(clear_checkbox_label)s</label>'
+        '<div class="fileinput">'
+        '   <a href="%(initial_url)s">%(initial)s</a> <br />'
+        '   <div class="clear">'
+        '      %(clear_template)s<br />'
+        '   </div>'
+        '   <div class="change">'
+        '       %(input_text)s: %(input)s'
+        '   </div>'
         '</div>'
     )
