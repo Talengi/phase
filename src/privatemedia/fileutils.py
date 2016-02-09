959a748c1562649fba5629c7e7c88443137f1a67
Thibault Jouannic <thibault@miximum.fr>
2016-02-09 12:13:39 +0100

    Migrations compatibility import

---
+++ src/privatemedia/fileutils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+# We need those imports for migrations compatibility purpose
+from privatemedia.storage import ProtectedStorage, PrivateStorage  # noqa
