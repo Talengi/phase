e02811d5bf9d5793ba545ebceafc29d4537745a8
Thibault Jouannic <thibault@miximum.fr>
2016-02-24 11:35:07 +0100

    Remove duplicate code

--- src/documents/models.py
+++ src/documents/models.py
@@ -638,8 +638,5 @@ class MetadataRevision(MetadataRevisionBase):
     received_date = models.DateField(
         _('Received date'))
 
-    class Meta:
-        abstract = True
-        ordering = ('-revision',)
-        get_latest_by = 'revision'
-        unique_together = ('document', 'revision')
+    class Meta(MetadataRevisionBase.Meta):
+        pass
