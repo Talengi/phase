3980e5b5372e7ea11d2c2d895f899f4e2cbf4925
Thibault Jouannic <thibault@miximum.fr>
2016-02-25 15:10:17 +0100

    Generate migration for entity modification

---
+++ src/accounts/migrations/0007_auto_20160225_1510.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0006_auto_20160208_1529'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='entity',
+            name='type',
+            field=models.CharField(default='contractor', max_length=80, verbose_name='Type', choices=[('contractor', 'Contractor'), ('supplier', 'Supplier'), ('originator', 'Originator'), ('other', 'Other')]),
+        ),
+    ]
