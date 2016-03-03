93b013b07cfc53c79a7099f1d7321d93659100cc
Laurent Paoletti <lp@providenz.fr>
2016-03-03 14:39:51 +0100

    Add mail settings fields

---
+++ src/accounts/migrations/0008_auto_20160303_1439.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0007_auto_20160225_1510'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='send_closed_reviews_mails',
+            field=models.BooleanField(default=True, verbose_name='Send mails when reviews are closed'),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='send_pending_reviews_mails',
+            field=models.BooleanField(default=True, verbose_name='Send pending reviews reminders'),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='send_trs_reminders_mails',
+            field=models.BooleanField(default=True, verbose_name='Send reminders for trs with missing ack of receipt'),
+        ),
+    ]
---
+++ src/transmittals/migrations/0055_auto_20160303_1439.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import transmittals.fields
+import privatemedia.storage
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('transmittals', '0054_outgoingtransmittal_archived_pdf'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='outgoingtransmittal',
+            name='archived_pdf',
+            field=transmittals.fields.OgtFileField(storage=privatemedia.storage.ProtectedStorage(), upload_to=transmittals.fields.ogt_file_path, null=True, verbose_name='Archived PDF', blank=True),
+        ),
+    ]
