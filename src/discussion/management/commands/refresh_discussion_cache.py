492564cd419ffb83ce2987a73518d1bceb0e192f
Laurent Paoletti <lp@providenz.fr>
2016-03-08 10:46:06 +0100

    Add management command to refresh cached discussions lengths

---
+++ src/discussion/management/commands/refresh_discussion_cache.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from discussion.models import Note
+from discussion.signals import update_cache
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """For each discussion, the discussion length is cached by a post_save
+    signal. If necessary, this command updates cache."""
+    def handle(self, *args, **options):
+        logger.info('Refresh discussion cache count')
+
+        for note in Note.objects.all():
+            update_cache(Note, note)
