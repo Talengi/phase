b354eace9361c1b7b47f12831e34d7b917d0151b
Laurent Paoletti <lp@providenz.fr>
2016-03-16 11:57:21 +0100

    Enable admin for audit log

--- src/audit_trail/admin.py
+++ src/audit_trail/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from documents.admin import NonEditableAdminMixin
+from .models import Activity
+
+
+class ActivityAdmin(NonEditableAdminMixin, admin.ModelAdmin):
+    actions = None
+    list_display = ('created_on', 'get_actor', 'verb', 'get_target', 'get_action_object')
+
+    def has_delete_permission(self, obj):
+        return False
+
+    def get_actor(self, obj):
+        actor = obj.actor or obj.actor_object_str
+        return actor
+    get_actor.short_description = 'Actor'
+
+    def get_target(self, obj):
+        target = obj.target or obj.target_object_str
+        return target
+    get_target.short_description = 'Target'
+
+    def get_action_object(self, obj):
+        action_object = obj.action_object or obj.action_object_str
+        return action_object
+    get_action_object.short_description = 'Action Object'
+
+admin.site.register(Activity, ActivityAdmin)
+
