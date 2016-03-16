2a0d54477bcfef608cd0349fb59a0269ddd724c5
Laurent Paoletti <lp@providenz.fr>
2016-03-16 11:43:34 +0100

    Add audit_trail signal and test

---
+++ src/audit_trail/signals.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.contenttypes.models import ContentType
+from django.dispatch import Signal, receiver
+from accounts.models import User
+
+activity_log = Signal(
+    providing_args=[
+        'actor',
+        'verb',
+        'action_object',
+        'action_type',
+        'target']
+)
+
+
+@receiver(activity_log)
+def activity_handler(verb, target, **kwargs):
+    from .models import Activity
+
+    kwargs.pop('signal', None)
+
+    if verb not in zip(*Activity.VERB_CHOICES)[0]:
+        raise ValueError("Verb must belong to Activity verbs")
+
+    activity = Activity()
+    activity.verb = verb
+
+    activity.target = target
+    activity.target_object_str = str(target)
+
+    actor = kwargs.pop('actor')
+    if isinstance(actor, User):
+        activity.actor_content_type = ContentType.objects.get_for_model(actor)
+        activity.actor_object_id = actor.pk
+        activity.actor_object_str = str(actor)
+    elif actor in Activity.NON_DB_USERS:
+        activity.actor_object_str = actor
+    else:
+        raise ValueError("Actor must be a user instance or belong to "
+                         "Activity NON_DB_USERS")
+
+    action_object = kwargs.pop('action_object', None)
+    if action_object:
+        activity.action_object = action_object
+        action_object.action_object_str = str(action_object)
+
+    activity.save()
+
---
+++ src/audit_trail/tests/tests_signals.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from accounts.factories import UserFactory
+from documents.factories import DocumentFactory
+
+from ..models import Activity
+from ..signals import activity_log
+
+
+class ActivitySignalTests(TestCase):
+    def test_signal(self):
+        doc = DocumentFactory()
+        user = UserFactory()
+        self.assertEqual(Activity.objects.count(), 0)
+        activity_log.send(verb='updated', target=doc, sender='self', actor=user)
+
+        latest_activity = Activity.objects.all().get()
+        self.assertEqual(latest_activity.verb, 'updated')
+        self.assertEqual(latest_activity.actor, user)
+        self.assertEqual(latest_activity.actor_object_str, str(user))
+        self.assertEqual(latest_activity.target, doc)
+        self.assertEqual(latest_activity.target_object_str, str(doc))
