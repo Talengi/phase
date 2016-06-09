2b8431d39dbbd1abfe4153a43efb8c37b970f31e
Laurent Paoletti <lp@providenz.fr>
2016-06-09 13:36:54 +0200

    Add test for clearmedia management command

---
+++ src/privatemedia/tests/test_command.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+
+from accounts.factories import UserFactory
+from categories.factories import CategoryFactory
+from default_documents.factories import MetadataRevisionFactory
+from documents.factories import DocumentFactory
+
+
+@override_settings(USE_X_SENDFILE=True)
+class CleanMediaTests(TestCase):
+    def setUp(self):
+        self.category = CategoryFactory()
+        user = UserFactory(
+            email='test@phase.fr',
+            password='pass',
+            is_superuser=True,
+            category=self.category)
+        self.client.login(email=user.email, password='pass')
+        self.doc = DocumentFactory()
+        self.rev = self.doc.get_latest_revision()
+
+    def test_command(self):
+        document = DocumentFactory(
+            category=self.category,
+        )
+        sample_path = b'documents/tests/'
+        native_doc = b'sample_doc_native.docx'
+        pdf_doc = b'sample_doc_pdf.pdf'
+
+        MetadataRevisionFactory(
+            metadata=document.get_metadata(),
+            revision=2,
+            native_file=SimpleUploadedFile(native_doc,
+                                           sample_path + native_doc),
+            pdf_file=SimpleUploadedFile(pdf_doc, sample_path + pdf_doc),
+        )
+        # Document without files
+        DocumentFactory(
+            category=self.category,
+        )
+        rev = document.get_latest_revision()
+        filepath = rev.pdf_file.path
+        # check file is on disk after doc deletion
+        document.delete()
+        self.assertTrue(os.path.exists(filepath))
+
+        call_command('clearmedia')
+        # command has deleted file
+        self.assertFalse(os.path.exists(filepath))
