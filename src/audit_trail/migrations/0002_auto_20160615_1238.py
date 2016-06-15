340769c456254dac172d7cc65c0cb3a38183ea16
Laurent Paoletti <lp@providenz.fr>
2016-06-15 12:40:03 +0200

    Generate audit trail missing migration

---
+++ src/audit_trail/migrations/0002_auto_20160615_1238.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('audit_trail', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='activity',
+            name='verb',
+            field=models.CharField(default='joined', max_length=128, verbose_name='Verb', choices=[('created', 'created'), ('edited', 'edited'), ('deleted', 'deleted'), ('joined', 'joined Phase'), ('started_review', 'started review'), ('cancelled_review', 'cancelled review'), ('reviewed', 'reviewed'), ('closed_reviewer_step', 'closed reviewer step'), ('closed_leader_step', 'closed leader step'), ('closed_approver_step', 'closed approver step'), ('sent_back_to_leader_step', 'sent review back to leader')]),
+        ),
+    ]
