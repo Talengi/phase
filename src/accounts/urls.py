3136802d59e81472916230512b5bdfc51066e0e0
Thibault Jouannic <thibault@miximum.fr>
2017-04-20 10:26:16 +0200

    Update url syntax

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,23 +1,25 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 from .forms import EmailAuthenticationForm
 
 
 urlpatterns = [
     url('^login/$',
-        'django.contrib.auth.views.login',
+        auth_views.login,
         {'authentication_form': EmailAuthenticationForm},
         name='login'),
     url('^logout/$',
-        'django.contrib.auth.views.logout_then_login',
+        auth_views.logout_then_login,
         name='logout'),
     url('^password-reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
+        auth_views.password_reset_confirm,
         name='password_reset_confirm'),
     url('^password-reset-complete/$',
-        'django.contrib.auth.views.password_reset_complete',
+        auth_views.password_reset_complete,
         name='password_reset_complete'),
     url(r'^password-change/$',
-        'django.contrib.auth.views.password_change',
+        auth_views.password_change,
         {'post_change_redirect': '/'},
         name='password_change')
 ]
