203145e9a6cea6802dc90b9e2376f99963e397c7
Thibault Jouannic <thibault@miximum.fr>
2017-05-22 12:17:51 +0200

    Convert squash migration to normal migration

--- src/exports/migrations/0001_squashed_0003_auto_20150915_1132.py
+++ src/exports/migrations/0001_squashed_0003_auto_20150915_1132.py
@@ -9,8 +9,6 @@ import uuid
 
 class Migration(migrations.Migration):
 
-    replaces = [(b'exports', '0001_initial'), (b'exports', '0002_export_status'), (b'exports', '0003_auto_20150915_1132')]
-
     dependencies = [
         ('categories', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
