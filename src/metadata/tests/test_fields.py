98ea9f9ff66feb109db3ae51003bb39343d9a084
Thibault Jouannic <thibault@miximum.fr>
2017-05-23 10:10:09 +0200

    Remove deleted method

--- src/metadata/tests/test_fields.py
+++ src/metadata/tests/test_fields.py
@@ -21,7 +21,7 @@ class ConfigurableChoiceFieldTest(TestCase):
 
     def test_choices_from_list(self):
         choices = get_choices_from_list(self.values_list.index)
-        self.assertItemsEqual(choices, [
+        self.assertCountEqual(choices, [
             ('test1', 'test1 - Test 1'),
             ('test2', 'test2 - Test 2'),
             ('test3', 'test3 - Test 3'),
