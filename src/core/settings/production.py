e375a94c07890f5ec7b00c59cff7dbe699e8b433
Thibault Jouannic <thibault@miximum.fr>
2017-06-19 16:51:02 +0200

    Update prod template settings

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -42,14 +42,7 @@ TEMPLATES = [
         'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
-            'loaders': [
-                ('django.template.loaders.cached.Loader', (
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                )),
-            ],
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
