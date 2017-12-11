b76982be5da1ca69cb1a627487afb9113c4f3821
Thibault Jouannic <thibault@miximum.fr>
2017-12-11 10:17:36 +0100

    Fix a bug in the revision file naming method

--- src/documents/fileutils.py
+++ src/documents/fileutils.py
@@ -4,9 +4,10 @@ def revision_file_path(revision, filename):
     based on the document number and the revision.
 
     """
+    has_status = bool(getattr(revision, 'status', None))
     return "revisions/{key}_{revision}{status}.{extension}".format(
         key=revision.document.document_key,
         revision=revision.name,
         extension=filename.split('.')[-1],
-        status="_" + revision.status if hasattr(revision, 'status') else ''
+        status="_" + revision.status if has_status else ''
     )
